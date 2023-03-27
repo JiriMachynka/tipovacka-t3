041943b9d9d32bca2608f075bbf75c50e2dc3ad0
Jiří Machynka <machynka.j@gmail.com>
2023-03-27 19:51:56 +0200

    Fix trpc error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -44,7 +42,6 @@ const Home: NextPage = () => {
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
         </div>
       </main>
