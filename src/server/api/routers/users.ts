78599acc177805b17d1fb3c74090cb2940420225
Jiří Machynka <machynka.j@gmail.com>
2023-05-01 18:01:24 +0200

    Unused code

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,6 +1,4 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
@@ -9,17 +7,4 @@ export const userRouter = createTRPCRouter({
       const userList = await clerkClient.users.getUserList();
       return userList;
     }),
-  getUser: publicProcedure
-    .input(z.object({
-      username: z.string()
-    }))
-    .query(async ({ ctx, input }) => {
-      const { username } = input;
-      const user = await ctx.prisma.player.findFirst({
-        where: {
-          username
-        }
-      });
-      return user;
-    })
 });
